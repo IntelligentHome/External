77ea5c6db436ce97bb085a79be722def177e781b
Mariusz Sikora <msikora87@gmail.com>
2016-12-06 21:45:01 +0100

    Set Auto Ack

--- include/INrf24.h
+++ include/INrf24.h
@@ -37,6 +37,7 @@ public:
     virtual Status SetAddress(Rx rx, const uint8_t address[], uint8_t size) = 0;
     virtual Status SetAddress(Tx tx, const uint8_t address[], uint8_t size) = 0;
     virtual Status SetPayload(const uint8_t payload[], const uint8_t size) = 0;
+    virtual Status SetAutoAck(const uint8_t auto_ack) = 0;
 
     virtual NrfStatusRegister GetStatus(void) = 0;
     virtual Status GetPayload(uint8_t payload[], const uint8_t size) = 0;
