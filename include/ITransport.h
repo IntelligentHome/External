65e881c71050b20a12486f868d004fcd41e279d0
Mariusz Sikora <msikora87@gmail.com>
2016-12-10 18:50:17 +0100

    Move Mocks to External/testing

--- include/ITransport.h
+++ include/ITransport.h
@@ -8,9 +8,9 @@ class ITransport {
 
 public:
 
-        virtual void Send(uint8_t send_buff[], uint16_t size) = 0;
-        virtual void Get(uint8_t recv_buff[], uint16_t size) = 0;
-        virtual void SendAndGet(uint8_t send_buff[], uint8_t recv_buff[], uint16_t size) = 0;
+    virtual void Send(uint8_t send_buff[], uint16_t size) = 0;
+    virtual void Get(uint8_t recv_buff[], uint16_t size) = 0;
+    virtual void SendAndGet(uint8_t send_buff[], uint8_t recv_buff[], uint16_t size) = 0;
 
 };
 
