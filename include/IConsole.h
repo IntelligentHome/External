87178a6ef3a731432fadf295975adf26b92b1faf
Mariusz Sikora <msikora87@gmail.com>
2016-12-20 20:41:14 +0100

    Interface for Console

---
+++ include/IConsole.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace iconsole {
+
+class IConsole {
+
+public:
+
+    virtual IConsole& print(const char* str) = 0;
+    virtual IConsole& print(const int8_t value) = 0;
+    virtual IConsole& newline(void) = 0;
+};
+
+} /*namespace IConsole*/
